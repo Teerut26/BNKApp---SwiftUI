75ff63cdf763124a6e27764d7b4c2412c60d7f1d
Teerut26 <teerutsr@gmail.com>
2024-11-23 00:05:38 +0700

    Initial Commit

---
+++ bnk/bnkApp.swift
@@ -0,0 +1,17 @@
+//
+//  bnkApp.swift
+//  bnk
+//
+//  Created by Teerut Srithongdee on 23/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct bnkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bnkTests/bnkTests.swift
@@ -0,0 +1,16 @@
+//
+//  bnkTests.swift
+//  bnkTests
+//
+//  Created by Teerut Srithongdee on 23/11/2567 BE.
+//
+
+import Testing
+
+struct bnkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
